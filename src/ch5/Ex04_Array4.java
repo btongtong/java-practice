1744468f867fb8d1aec157d196a558d492dc6322
btongtong <leeym2615@naver.com>
2024-04-23 00:19:39 +0900

    자바의 정석 ch4(Array)

---
+++ src/ch5/Ex04_Array4.java
@@ -0,0 +1,22 @@
+package ch5;
+
+public class Ex04_Array4 {
+    public static void main(String[] args) {
+        char[] abc = {'a', 'b', 'c'};
+        char[] num = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        System.out.println(abc);
+        System.out.println(num);
+
+        char[] result = new char[abc.length + num.length];
+        System.arraycopy(abc, 0, result, 0, abc.length);
+        System.arraycopy(num, 0, result, abc.length, num.length);
+        System.out.println(result);
+
+        System.arraycopy(abc, 0, num, 0, abc.length);
+        System.out.println(num);
+
+        System.arraycopy(abc, 0 , num, 6, abc.length);
+        System.out.println(num);
+
+    }
+}
